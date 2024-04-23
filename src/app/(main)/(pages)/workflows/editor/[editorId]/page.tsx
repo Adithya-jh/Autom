4d0a719eafa094eaa11c9a6df58a295e2e7c3c15
Adithya <jhadith88@gmail.com>
2024-04-23 14:34:48 +0530

    editor goingon

--- src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -2,6 +2,7 @@ import { ConnectionsProvider } from '../../../../.././../providers/connections-p
 import EditorProvider from '../../../../.././../providers/editor-provider';
 import React from 'react';
 import EditorCanvas from './_components/editor-canvas';
+// import EditorCanvas from './_components/editor-canvas';
 
 type Props = {};
 
